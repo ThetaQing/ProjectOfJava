2722642689cdfe0665b2eae948679dafa8273c5c
ThetaQing <csusq2016@163.com>
2020-09-09 19:03:15 +0800

    创建线程——实现Runnable接口

---
+++ TEST/src/songqing/lesson3/TestThread3.java
@@ -0,0 +1,26 @@
+package songqing.lesson3;
+
+// 创建线程方式2：实现Runnable接口，重写run方法，执行线程需要丢入Runnable接口实现类，调用start方法。
+public class TestThread3 implements Runnable{
+    @Override
+    public void run() {
+        // run方法线程体
+        for (int i = 0; i < 200; i++) {
+            System.out.println("-----------: " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Begin");
+        // 创建Runnable接口的实现类对象
+        TestThread3 thread1 = new TestThread3();
+        // 创建线程对象，通过线程对象来开启线程，代理
+        Thread thread = new Thread(thread1);
+        // 启动线程
+        thread.start();
+
+
+        System.out.println("End");
+
+    }
+}
